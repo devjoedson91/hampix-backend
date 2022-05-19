b464f64dfc671b86f5bc39a2cea8ce193a041cd2
Joedson Ferreira <joedson.devel@gmail.com>
2022-05-19 10:24:32 -0300

    criando middlwares para rota de usuarios

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,25 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
+
+import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+
+import { isAuthenticated } from './middlewares/isAuthenticated'; // middleware
 
 const router = Router();
 
-router.get('/test', (req: Request, res: Response) => { // (typescript) req é do tipo Resquest e permite saber o que tem no Resquest
+// ROTAS USERS
 
-    return res.json({ok: true});
+router.post('/users', new CreateUserController().handle); // chamando o controller, criando user
 
-});
+router.post('/session', new AuthUserController().handle); // chamando o authuser, autenticando o user
+
+router.get('/userinfo', isAuthenticated, new DetailUserController().handle); 
+
+// antes de chamar a classe detailuser, passe pelo um middleware que verifica o token é valido e deixa prosseguir ou nao
+
+// ROTAS CATEGORY
+
+
 
 export { router };
