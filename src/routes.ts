9d2fe4c39da4df7dd048109ea7c87f0301064e69
Joedson Ferreira <joedson.devel@gmail.com>
2022-05-19 12:05:03 -0300

    Criando e listando categorias

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,9 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController';
+
 import { isAuthenticated } from './middlewares/isAuthenticated'; // middleware
 
 const router = Router();
@@ -18,7 +21,19 @@ router.get('/userinfo', isAuthenticated, new DetailUserController().handle);
 
 // antes de chamar a classe detailuser, passe pelo um middleware que verifica o token é valido e deixa prosseguir ou nao
 
+
 // ROTAS CATEGORY
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle);
+
+
+// ROTAS PRODUCTS
+
+
+
+
 
 
 
